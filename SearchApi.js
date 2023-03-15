63f2df3b3c634cf34ad7fa02e3fb451c22567cbc
Denise Buckhana <57542774+mulraind001@users.noreply.github.com>
2023-03-15 12:43:37 -0400

    Made the search results a full width

--- SearchApi.js
+++ SearchApi.js
@@ -1,20 +1,19 @@
 const SerpApi = require('google-search-results-nodejs');
 const search = new SerpApi.GoogleSearch("aa4554a480f33e3b4a718975f30f52b342e56ae36de2c48c355a68588e2c7a94");
+var query
 
 const params = {
     engine: "google",
-    q: "Coffee",
-    location: "Austin, Texas, United States",
+    q: query,
     google_domain: "google.com",
     gl: "us",
     hl: "en",
     safe: "active",
-    tbm: "isch",
-    ijn: "10"
+    tbm: "isch"
 };
 
 const callback = function(data) {
-    console.log(data);
+    console.log(data["image_results"]);
 };
 
 // Show result as JSON
